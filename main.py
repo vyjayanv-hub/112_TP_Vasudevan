df4fabf5be358b153e95f0521025b61c57652ed4
Vyju Vasudevan <vyjayanv@andrew.cmu.edu>
2026-04-20 13:05:15 -0400

    Initial commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from cmu_graphics import *
 
 def onAppStart(app):
-    app.height = 600
-    app.width = 410
+    app.height = 890
+    app.width = 680
     app.margin = 30
     app.lineHeight = 18
     app.fontSize = 14
+    app.books = dict()
     app.fileName = 'pg844.txt'
     rawText = loadBook(app.fileName)
     
@@ -15,25 +16,80 @@ def onAppStart(app):
     app.pages = makePages(rawText, app.charsPerPage)
     app.pageIndex = 0
     app.totalPages = len(app.pages)
-    
-    app.reading = True
-    app.defaultScreen = False
-    app.showCursor = True
+    makeButtons(app)
+    app.readingScreen = False
+    app.defaultScreen = True
+    app.usingCamera = True
     app.cursorX = 200
     app.cursorY = 200
     app.offset = 10
     app.curvedEdgeRadius = 10
     app.curvedEdgeOffset = 19
     app.burgerPressed = False
+    app.displayScreenOffsetY = 6
+    app.displayScreenOffsetX = 8
+    app.displayScreenTop = 90
+    app.displayScreenLeft = 88
+    app.displayScreenWidth = 490
+    app.displayScreenHeight = 650
 
+
+    app.menuWidth = 100
+    app.burgerAndMenuOffset = 22
+    app.menuHeight = 180
+
+def makeBooks(app):
+    app.earnestWilde = Books(100, 100, 'The Picture of Dorian Gray', 'Oscar Wilde', 254, 'pg844.cover.medium.jpg')
+    app.littleWomenAlcott = Books(100, 200, 'Little Women', 'Louisa May Alcott', 499, 'pg25344.cover.medium.jpg')
+    app.gatsbyFitzgerald = Books(100, 300, 'The Great Gatsby', 'Scott Fitzgerald', 180, 'pg64317.cover.medium.jpg')
+    app.scarletHawthorne = Books(250, 100, 'The Scarlet Letter', 'Nathaniel Hawthorne', 272,'pg25344.cover.medium.jpg')
+
+def makeButtons(app):
+    app.buttons = []
+    darkPastelBlue = rgb(115, 115, 115)
+
+    app.libraryButton = Buttons(app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX,
+                                app.displayScreenTop + app.displayScreenOffsetY + app.burgerAndMenuOffset, 
+                                app.menuWidth - 2.5, app.menuHeight//4,
+                                'white', darkPastelBlue, 'Library', None )
+    app.buttons.append(app.libraryButton)
 
 def onKeyPress(app, key):
     if key == 'right' and app.pageIndex < app.totalPages - 1:
         app.pageIndex += 1
     elif key == 'left' and app.pageIndex > 0:
         app.pageIndex -= 1
-##################
+
+
 def onMousePress(app, mouseX, mouseY):
+    if app.readingScreen:
+        if app.usingCamera:
+            readingCamera(app)
+            ####################make readingCamera
+        readingMousePress(app, mouseX, mouseY)
+    if app.defaultScreen:
+        checkBurgerPressed(app, mouseX, mouseY)
+        app.libraryButton = getButtonPressed(app, mouseX, mouseY)
+
+def getButtonPressed(app, mouseX, mouseY):
+    intersected = []
+    for button in app.buttons:
+        if button.intersect(mouseX, mouseY):
+            intersected.append(button)
+    return intersected
+
+
+def checkBurgerPressed(app, mouseX, mouseY):
+    hamburgerLineLength = 17
+    spacing = 6
+    upperY = app.displayScreenTop + app.displayScreenOffsetY
+    lowerY = app.displayScreenTop + app.displayScreenOffsetY + spacing*2
+    upperX = app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX
+    lowerX = app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX - hamburgerLineLength
+    if (lowerX <= mouseX <= upperX) and (upperY <= mouseY <= lowerY):
+        app.burgerPressed = not app.burgerPressed
+
+def readingMousePress(app, mouseX, mouseY):
     if mouseX > app.width /2:
         if app.pageIndex < app.totalPages - 1:
             app.pageIndex += 1
@@ -45,22 +101,19 @@ def redrawAll(app):
     if app.defaultScreen:
         drawDefaultScreen(app)
         drawHamburgerButton(app)
-    if app.reading:
+    if app.readingScreen:
         drawBook(app)
-    if app.showCursor:
-        #insert image later
-        drawCircle(app.cursorX, app.cursorY, 10, fill='darkBlue')
+    #cursor
+    drawCircle(app.cursorX, app.cursorY, 10, fill='darkBlue')
+    if app.burgerPressed:
+        drawBurgerMenu(app)
         
 def drawBook(app):
     drawRect(0,0, app.width, app.height, fill = 'ivory')
-    
-    drawRect(0, 0, app.width, 50, fill='darkSlateGray')
-    drawLabel('My Gutenberg Reader', app.width / 2, 25, fill = 'white', size = 18, bold=True)
     pageText = app.pages[app.pageIndex]
     drawWrappedText(app, pageText)
-    
-    drawRect(0, app.height - 40, app.width, 40, fill='darkSlateGray')
     drawLabel(f'Page {app.pageIndex + 1} of {app.totalPages}', app.width / 2, app.height - 20, fill='white', size = 13)
+
 def drawWrappedText(app, text):
     maxWidth = app.width - 2*app.margin
     charsPerLine = int(maxWidth / (app.fontSize * 0.55))
@@ -84,12 +137,6 @@ def drawWrappedText(app, text):
         y += app.lineHeight //2
         if y > app.height - 50:
             return
-    
-    
-    ################################
-
-
-
 
 def onMouseMove(app, mouseX, mouseY):
     app.cursorX = mouseX
@@ -128,13 +175,7 @@ def makePages(text, charsPerPage = 800):
         pages.append(current.strip())
     return pages
 
-def drawAllButtons(app):
-    pastelDarkBlue = rgb(128,153,182)
-    #cursorButton = Button('',pastelDarkBlue , None, 'cmu://1166168/46485232/cursor--v1.jpg',40, 20, cx, cy, clicked)
-    #other button
-    cursorButton.drawButton()
-    #draw all buttons
-    
+
 def drawDefaultScreen(app):
     #make turqoise background
     turqoise = rgb(186, 221, 212)
@@ -146,49 +187,56 @@ def drawDefaultScreen(app):
     drawCircle(app.width - app.curvedEdgeOffset, app.height - app.curvedEdgeOffset, app.curvedEdgeRadius, fill = turqoise)
     #make gray
     
-    defaultScreenURL = 'cmu://1166168/46510460/Screenshot+2026-04-17+at+2.55.49 PM.png'
+    defaultScreenURL = 'defaultScreen.png'
     imageWidth, imageHeight = getImageSize(defaultScreenURL)
-    drawImage(defaultScreenURL, app.width//2, app.height//2 + 15, align='center', width=imageWidth//2, height=imageHeight//1.7)
+    drawImage(defaultScreenURL, app.width//2, app.height//2 + 15, align='center', width=imageWidth*1.5, height=imageHeight*1.5)
 
     lightGray = rgb(226, 226, 226)
     darkGray = rgb(133, 133, 133)
     
 def drawBurgerMenu(app):
-    menuWidth = 50
-    drawRect(app.width - 30 - menuWidth, 55, menuWidth, 60, fill='black')
+
+
+    drawRect(app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX - app.menuWidth,
+             app.displayScreenTop + app.displayScreenOffsetY + app.burgerAndMenuOffset, app.menuWidth, app.menuHeight, fill='black')
+    
+    app.libraryButton.drawButton()
+    
+
     
 def drawHamburgerButton(app):
     hamburgerLineLength = 17
     hamburgerColor = rgb(115, 115, 115)
     spacing = 6
-    startY = 65
-    startX = app.width - 45
-    drawLine(startX - hamburgerLineLength, startY, startX, startY, fill = hamburgerColor, lineWidth = 3)
-    drawLine(startX - hamburgerLineLength, startY + spacing, startX, startY + spacing, fill = hamburgerColor, lineWidth = 3)
-    drawLine(startX - hamburgerLineLength, startY + spacing*2, startX, startY + spacing*2,fill = hamburgerColor, lineWidth = 3)
-
+    drawLine(app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX - hamburgerLineLength, app.displayScreenTop + app.displayScreenOffsetY, app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX, app.displayScreenTop + app.displayScreenOffsetY, fill = hamburgerColor, lineWidth = 3)
+    drawLine(app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX - hamburgerLineLength, app.displayScreenTop + app.displayScreenOffsetY + spacing, app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX, app.displayScreenTop + app.displayScreenOffsetY + spacing, fill = hamburgerColor, lineWidth = 3)
+    drawLine(app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX - hamburgerLineLength, app.displayScreenTop + app.displayScreenOffsetY + spacing*2, app.displayScreenLeft + app.displayScreenWidth - app.displayScreenOffsetX, app.displayScreenTop + app.displayScreenOffsetY + spacing*2, fill = hamburgerColor, lineWidth = 3)
 class Buttons:
-    def __init__(self, textColor, backgroundColor, text,imageURL, width, height, cx, cy, clicked):
+    def __init__(self, left, top, width, height, textColor, backgroundColor, text,imageURL):
         self.textColor = textColor
         self.backgroundColor = backgroundColor
         self.text = text
         self.width = width
         self.height = height
-        self.cx = cx
-        self.cy = cy
-        self.URL = imageURL
-        self.clicked = clicked
+        self.left = left
+        self.top = top
+        if imageURL != None:
+            self.URL = imageURL
     
     def __repr__(self):
         return f'button:{self.text}'
     
+    def intersect(self, x, y):
+        upperX = self.width + self.left
+        upperY = self.height + self.top
+        return self.left <= x <= upperX and self.top <= y <= upperY
+
     def drawButton(self):
-        drawCircle(self.cx + self.width//2, self.cy - self.height//2, 10, fill = self.backgroundColor)
-        drawCircle(self.cx - self.width//2, self.cy - self.height//2, 10, fill = self.backgroundColor)
-        drawCircle(self.cx - self.width//2, self.cy + self.height//2, 10, fill = self.backgroundColor)
-        drawCircle(self.cx + self.width//2, self.cy + self.height//2, 10, fill = self.backgroundColor)
-        drawRect(self.cx - self.width//2, self.cy - self.height//2, self.width, self.height, fill = self.backgroundColor)
-        drawLabel(self.text, cx, cy, fill = self.textColor)
+        if None not in (self.left, self.top, self.width, self.height):
+            drawRect(self.left, self.top, self.width, self.height, fill = self.backgroundColor)
+            drawLabel(self.text, self.left - self.width//2, self.top + self.height//2, fill=self.textColor)
+        else:
+            raise Exception("Haven't set button size and position yet before drawing")
     
     def __hash__(self):
         return hash(str(self))
@@ -197,16 +245,23 @@ class Buttons:
         return isinstance(other, Buttons) and (self.text, self.backgroundColor, self.textColor) == (other.text, other.backgroundColor, other.textColor)
 
 class Books:
-    def __init__(self, left, top, title, author, numPages, currentlyReading, currPage, currentlyOpen):
-        self.currentlyReading = currentlyReading
+    def __init__(self, left, top, title, author, numPages, imageURL):
         self.left = left
         self.top = top
         self.title = title
         self.author = author
+        self.coverImage = imageURL
         self.numPages = numPages
-        self.currentlyReading = currentlyReading
-        self.currPage = currPage
-        self.currentlyOpen = currentlyOpen
+        self.currentlyReading = False
+        self.currPage = 0
+        self.currentlyOpen = False
+    
+    def drawCovers(self):
+        for book in app.library:
+            if book.coverImage != None:
+                imageWidth, imageHeight = getImageSize(self.coverImage)
+                drawImage(app.url, app.left, app.top, align='center',
+              width=imageWidth//2, height=imageHeight//2)
     
     def __repr__(self):
         return f'book: Author{self.author} Title{self.title}'
@@ -215,7 +270,7 @@ class Books:
         return hash(str(self))
     
     def __eq__(self, other):
-        return isinstance(other, book) and ((self.title, self.author) == (other.title, other.author))
+        return isinstance(other, Books) and ((self.title, self.author) == (other.title, other.author))
 
 
 
